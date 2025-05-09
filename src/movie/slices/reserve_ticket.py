e26601aef493ed515466401bfe092e6d1d6236ee
Chris May <chris@may.org>
2025-05-08 22:14:19 -0400

    add user to ticket submission

--- src/movie/slices/reserve_ticket.py
+++ src/movie/slices/reserve_ticket.py
@@ -6,4 +6,6 @@ bp = flask.Blueprint("reserve_ticket", __name__)
 
 @bp.post('/showing/<string:showing_id>')
 def reserve_ticket(showing_id: str):
-    return dict(seats=request.form.getlist("selected_seats"), showing=showing_id)
+    return dict(
+        seats=request.form.getlist("selected_seats"), showing=showing_id, form=request.form, user=request.form['user']
+    )
