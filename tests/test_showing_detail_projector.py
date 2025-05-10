d3ec451a6f0e606d9184a7c6e447ab2bb50d93bb
Chris May <chris@may.org>
2025-05-10 14:53:25 -0400

    fix the other tests

--- tests/test_showing_detail_projector.py
+++ tests/test_showing_detail_projector.py
@@ -1,97 +1,53 @@
-from datetime import datetime
-from uuid import uuid4
+import random
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import svcs
+from sqlalchemy.orm import Session
 
-from src.movie.slices.showing_detail import Base, ShowingDetail, ShowingDetailProjector
-
-
-class DummyEvent:
-    pass
-
-
-def make_session():
-    engine = create_engine('sqlite:///:memory:')
-    Base.metadata.create_all(engine)
-    return sessionmaker(bind=engine)()
+import movie.slices.showing_detail.model as showing_detail_model
+from movie.domain.model import Movie
+from movie.infrastructure.store import IEventStore
+from tests.factories import MovieAddedFactory, ShowingAddedFactory, TicketReservedFactory
 
 
 def test__showing_added__creates_row():
     # GIVEN a projector and a ShowingAdded event
-    session = make_session()
-    projector = ShowingDetailProjector(session)
-    event = DummyEvent()
-    event.entity_id = uuid4()
-    event.movie_name = "Inception"
-    event.movie_poster = "http://example.com/inception.jpg"
-    event.duration = 148
-    event.start_time = datetime(2023, 6, 15, 19, 30)
-    event.available_seats = ["A1", "A2", "A3"]
+    session, event_store = svcs.flask.get(Session, IEventStore)
+    movie_created = MovieAddedFactory.build()
+    movie = Movie(movie_created)
+    event_store.save(movie_created)
+    event = ShowingAddedFactory.build(movie_id=movie.movie_id)
 
     # WHEN the event is handled
-    projector.handle_showing_added(event)
+    showing_detail_model.handle_showing_added(event)
 
     # THEN a row is created with correct data
-    row = session.query(ShowingDetail).filter_by(showing_id=str(event.entity_id)).first()
+    row = session.query(showing_detail_model.ShowingDetail).filter_by(showing_id=str(event.entity_id)).first()
     assert row is not None
-    assert row.movie_name == "Inception"
-    assert row.poster_url == "http://example.com/inception.jpg"
-    assert row.duration == 148  # noqa: PLR2004
-    assert row.start_time == datetime(2023, 6, 15, 19, 30)
-    assert row.available_seats == "A1,A2,A3"
+    assert row.movie_name == movie.title
+    assert row.poster_url == movie.poster_url
+    assert row.duration == movie.duration
+    assert row.start_time == event.start_time
+    assert row.available_seats == ','.join(event.available_seats)
     assert row.reserved_seats == ""
-    assert row.all_seats == "A1,A2,A3"
+    assert row.all_seats == ','.join(event.available_seats)
 
 
 def test__ticket_reserved__moves_seat():
-    # GIVEN a showing row and a TicketReserved event
-    session = make_session()
-    projector = ShowingDetailProjector(session)
-    # Add showing
-    event = DummyEvent()
-    event.entity_id = uuid4()
-    event.movie_name = "Inception"
-    event.movie_poster = "http://example.com/inception.jpg"
-    event.duration = 148
-    event.start_time = datetime(2023, 6, 15, 19, 30)
-    event.available_seats = ["A1", "A2", "A3"]
-    projector.handle_showing_added(event)
-    # Reserve seat A2
-    reserve_event = DummyEvent()
-    reserve_event.showing_id = event.entity_id
-    reserve_event.seat_id = "A2"
-    projector.handle_ticket_reserved(reserve_event)
-    # THEN A2 is moved from available to reserved
-    row = session.query(ShowingDetail).filter_by(showing_id=str(event.entity_id)).first()
-    assert set(row.available_seats.split(",")) == {"A1", "A3"}
-    assert set(row.reserved_seats.split(",")) == {"A2"}
+    # GIVEN a showing row and a TicketReserved showing_added
+    session, event_store = svcs.flask.get(Session, IEventStore)
+    movie_created = MovieAddedFactory.build()
+    movie = Movie(movie_created)
+    event_store.save(movie_created)
+    showing_added = ShowingAddedFactory.build(movie_id=movie.movie_id)
+    event_store.save(movie_created, showing_added)
+    showing_detail_model.handle_showing_added(showing_added)
 
+    # When we reserve a seat
+    seat = random.choice(showing_added.available_seats)  # noqa: S311
+    reserve_event = TicketReservedFactory.build(entity_id=showing_added.entity_id, seat_id=seat)
+    showing_detail_model.handle_ticket_reserved(reserve_event)
 
-def test__ticket_cancelled__moves_seat_back():
-    # GIVEN a showing row with a reserved seat and a TicketCancelled event
-    session = make_session()
-    projector = ShowingDetailProjector(session)
-    # Add showing
-    event = DummyEvent()
-    event.entity_id = uuid4()
-    event.movie_name = "Inception"
-    event.movie_poster = "http://example.com/inception.jpg"
-    event.duration = 148
-    event.start_time = datetime(2023, 6, 15, 19, 30)
-    event.available_seats = ["A1", "A2", "A3"]
-    projector.handle_showing_added(event)
-    # Reserve seat A2
-    reserve_event = DummyEvent()
-    reserve_event.showing_id = event.entity_id
-    reserve_event.seat_id = "A2"
-    projector.handle_ticket_reserved(reserve_event)
-    # Cancel seat A2
-    cancel_event = DummyEvent()
-    cancel_event.showing_id = event.entity_id
-    cancel_event.seat_id = "A2"
-    projector.handle_ticket_cancelled(cancel_event)
-    # THEN A2 is back in available, not in reserved
-    row = session.query(ShowingDetail).filter_by(showing_id=str(event.entity_id)).first()
-    assert set(row.available_seats.split(",")) == {"A1", "A2", "A3"}
-    assert row.reserved_seats == ""
+    # THEN the seat is moved to reserved
+    row = session.query(showing_detail_model.ShowingDetail).filter_by(showing_id=str(showing_added.entity_id)).first()
+    assert seat not in set(row.available_seats.split(","))
+    assert row.reserved_seats == seat
