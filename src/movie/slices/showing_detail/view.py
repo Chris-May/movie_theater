407a3db3003defb9f9acbe2b8cb7c9dd83489f0b
Chris May <chris.may@wellsky.com>
2025-05-13 14:53:07 -0400

    clean up code

--- src/movie/slices/showing_detail/view.py
+++ src/movie/slices/showing_detail/view.py
@@ -1,10 +1,9 @@
 import asyncio
-import json
 
 import quart
 from datastar_py import ServerSentEventGenerator
 from datastar_py.quart import make_datastar_response
-from quart import current_app, request
+from quart import current_app
 from sqlalchemy.orm import Session
 
 from movie import services
@@ -26,16 +25,6 @@ async def get_showing(showing_id) -> ShowingDetail:
     return session.query(ShowingDetail).filter_by(showing_id=showing_id).scalar()
 
 
-async def showing_info(showing_id):
-    showing = await get_showing(showing_id)
-    user_id = services.get(UserID)
-    datastar_stuff = json.loads((await request.values).get('datastar', '{}'))
-    selected_seats = {key for key, val in datastar_stuff.get('selected-seats', {}).items() if val} - set(
-        showing.reserved_seats
-    )
-    return selected_seats, showing, user_id, showing.reserved_seats
-
-
 @bp.get('/showing/<string:showing_id>/updates')
 async def detail_updates(showing_id):
     app = current_app._get_current_object()  # noqa: SLF001
@@ -44,8 +33,7 @@ async def detail_updates(showing_id):
         while True:
             async with app.app_context():
                 showing = await get_showing(showing_id)
-                reserved_seats = showing.reserved_seats.split(',') if showing.reserved_seats else []
-                yield ServerSentEventGenerator.merge_signals(dict(reservedSeats=reserved_seats))
+                yield ServerSentEventGenerator.merge_signals(dict(reservedSeats=showing.reserved_seats))
                 await asyncio.sleep(1)
 
     response = await make_datastar_response(updates())
