7aa1001f67c0184fcb79b1aadf90884c0f350fa0
Chris May <chris@may.org>
2025-05-14 15:07:56 -0400

    remove selected seat from list when reserved

--- src/movie/slices/showing_detail/view.py
+++ src/movie/slices/showing_detail/view.py
@@ -16,14 +16,14 @@ bp = quart.Blueprint('detail_showing_view', __name__)
 
 
 @bp.get('/showing/<string:showing_id>')
-async def showing_detail(showing_id):
+async def load_page(showing_id):
     user_id = services.get(UserID)
     showing = await get_showing(showing_id)
     return await quart.render_template('showing_detail.html', showing=showing, user_id=user_id)
 
 
 @bp.get('/showing/<string:showing_id>/seats')
-async def update_seats(showing_id):
+async def on_seat_selection(showing_id):
     app = current_app._get_current_object()  # noqa: SLF001
     # Get the data once before entering the streaming context
     initial_selected = json.loads(request.args.get('seats'))
@@ -44,7 +44,11 @@ async def update_seats(showing_id):
                         'selectedSeats': valid_selected,
                     }
                 )
-                content = f"<ul id=seat-list>{''.join([f'<li>{seat}</li>' for seat in valid_selected])}</ul>"
+                content = (
+                    "<ul id=seat-list>"
+                    f"{''.join([f'<li id="selected-{seat}">{seat}</li>' for seat in valid_selected])}"
+                    "</ul>"
+                )
                 yield ServerSentEventGenerator.merge_fragments(
                     content, selector='#seat-list', merge_mode=FragmentMergeMode.OUTER
                 )
@@ -67,6 +71,8 @@ async def detail_updates(showing_id):
             async with app.app_context():
                 showing = await get_showing(showing_id)
                 yield ServerSentEventGenerator.merge_signals(dict(reservedSeats=showing.reserved_seats))
+                for seat in showing.reserved_list:
+                    yield ServerSentEventGenerator.remove_fragments(f'#selected-{seat}')
                 await asyncio.sleep(2)
 
     response = await make_datastar_response(updates())
